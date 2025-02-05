4f4dc5d606b2ba31a39a8ce9a0703bb08b6c8ab3
Newport911 <portnovphoto@gmail.com>
2025-02-05 21:11:53 +0300

    Переписал на асинхронные функции

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,20 +1,12 @@
 from pyrogram import Client, filters
 from pyrogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-
 from cachetools import TTLCache
-
 import os
 from dotenv import load_dotenv
-
-
-import requests
 import httpx
 from requests.auth import HTTPBasicAuth
 
-
 load_dotenv()
-
 
 API_ID = os.getenv("api_id")
 API_HASH = os.getenv("api_hash")
@@ -28,7 +20,6 @@ cache = TTLCache(maxsize=1, ttl=300)
 TIMEOUT = 30.0
 REQUEST_TIMEOUT = httpx.Timeout(TIMEOUT)
 
-
 app = Client(
     "my_bot",
     api_id=API_ID, api_hash=API_HASH,
@@ -36,7 +27,7 @@ app = Client(
 )
 
 @app.on_message(filters.command("start") & filters.private)
-def start(client, message):
+async def start(client, message):
     keyboard = ReplyKeyboardMarkup(
         [
             [KeyboardButton("Узнать статус заказа"), KeyboardButton("Нужна помощь")],
@@ -44,56 +35,50 @@ def start(client, message):
         ],
         resize_keyboard=True
     )
-    message.reply_text("Добро пожаловать! Выберите опцию:", reply_markup=keyboard)
+    await message.reply_text("Добро пожаловать! Выберите опцию:", reply_markup=keyboard)
 
 @app.on_message(filters.text & filters.private & filters.regex("^Узнать статус заказа$"))
-def status_order(client, message):
-    message.reply_text("Узнайте статус заказа")
+async def status_order(client, message):
+    await message.reply_text("Узнайте статус заказа")
 
 @app.on_message(filters.text & filters.private & filters.regex("^Нужна помощь$"))
-def help_request(client, message):
+async def help_request(client, message):
     manager_username = "phsquat_chat"  # Замените на имя пользователя менеджера
-    message.reply_text(f"Вы можете связаться с нашим менеджером по этой ссылке: https://t.me/{manager_username}")
-
-
+    await message.reply_text(f"Вы можете связаться с нашим менеджером по этой ссылке: https://t.me/{manager_username}")
 
 @app.on_message(filters.text & filters.private & filters.regex("^Наши соц сети$"))
-def social_media(client, message):
-    message.reply_text("Ссылка на наши соцсети")
+async def social_media(client, message):
+    await message.reply_text("Ссылка на наши соцсети")
 
-
-"""Кнопка отвечающая за получение списка доступных товаров. Если товары уже были загружены в кэш, они будут извлечены из
- кэша. Если нет, будут загружены с сервера и сохранены в кэш для быстрого доступа в будущем. Если возникла ошибка при 
- получении данных, будет отправлено сообщение об ошибке.")"""
 @app.on_message(filters.text & filters.private & filters.regex("^Пленка в наличии$"))
-def available_products(client, message):
+async def available_products(client, message):
     if "products" in cache:
         product_list = cache["products"]
         print("Подняли с кэша")
     else:
         try:
-            url = API_URL
-            response = requests.get(url, auth=HTTPBasicAuth(USERNAME, PASSWORD), timeout=TIMEOUT)
-            response.raise_for_status()
-            products = response.json()
-            product_list = "📦 **Доступные товары:**\n\n"
-            for product in products:
-                quantity = product['quantity']
-                if quantity < 2:
-                    quantity_text = "Осталась пара штук"
-                elif quantity < 10:
-                    quantity_text = "Осталась последние 10"
-                elif quantity < 20:
-                    quantity_text = "Много"
-                else:
-                    quantity_text = "Целая гора"
-                product_list += f"🔹 **Название:** {product['name']}\n💰 Цена: {product['price']}\n📊 Количество: {quantity_text}\n\n"
-            cache["products"] = product_list
-        except requests.exceptions.RequestException as e:
-            message.reply_text(f"Ошибка при получении данных, попробуйте позже {e}")
+            async with httpx.AsyncClient() as client:
+                response = await client.get(API_URL, auth=HTTPBasicAuth(USERNAME, PASSWORD), timeout=REQUEST_TIMEOUT)
+                response.raise_for_status()
+                products = response.json()
+                product_list = "📦 **Доступные товары:**\n\n"
+                for product in products:
+                    quantity = product['quantity']
+                    if quantity < 2:
+                        quantity_text = "Осталась пара штук"
+                    elif quantity < 10:
+                        quantity_text = "Осталась последние 10"
+                    elif quantity < 20:
+                        quantity_text = "Много"
+                    else:
+                        quantity_text = "Целая гора"
+                    product_list += f"🔹 **Название:** {product['name']}\n💰 Цена: {product['price']}\n📊 Количество: {quantity_text}\n\n"
+                cache["products"] = product_list
+        except httpx.RequestError as e:
+            await message.reply_text(f"Ошибка при получении данных, попробуйте позже {e}")
             return
 
-    message.reply_text(product_list)
+    await message.reply_text(product_list)
 
 if __name__ == "__main__":
     app.run()
